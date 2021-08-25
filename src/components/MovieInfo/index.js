920da326ec9093b9d09ced4524cda1433e119d88
kallelampinen <71073159+kallelampinen@users.noreply.github.com>
2021-08-25 10:58:29 +0200

    d

--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -24,6 +24,27 @@ const MovieInfo = ({ movie }) => (
         clickable={false}
         alt="movie thumb"
       ></Thumb>
+
+      <Text>
+        <h1>{movie.title}</h1>
+        <h3>PLOT</h3>
+        <p>{movie.overview}</p>
+
+        <div className="rating-directors">
+          <div>
+            <h3>RATING</h3>
+            <div className="score">{movie.vote_average}</div>
+          </div>
+          <div className="director">
+            <h3>
+              DIRECTOR{movie.directors.length > 1 ? "S" : ""}
+              {movie.directors.map((director) => (
+                <p key={director.credit_id}>{director.name}</p>
+              ))}
+            </h3>
+          </div>
+        </div>
+      </Text>
     </Content>
   </Wrapper>
 );
